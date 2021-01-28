f2c9846845feca08ba56d8c9641cda2eacdf692a
Aaron Dodd <ad19abg@herts.ac.uk>
2021-01-28 18:51:36 +0000

    Add CORS to server middleware

--- src/interface/server/src/server.js
+++ src/interface/server/src/server.js
@@ -1,7 +1,9 @@
+const cors = require('cors');
 const express = require('express');
 const api = require('./routes/api');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const port = process.env.port || 4000;
