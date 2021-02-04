e037c03e524b59a4acd4b0692dc59785aca040a6
Aaron Dodd <ad19abg@herts.ac.uk>
2021-02-04 20:18:59 +0000

    Update verification contract to check license for valid expiration date

--- src/contract/license/go/verification_contract.go
+++ src/contract/license/go/verification_contract.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"encoding/json"
+	"time"
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
@@ -10,8 +13,33 @@ type VerificationContract struct {
 
 func (vc *VerificationContract) VerifyOwnership(ctx contractapi.TransactionContextInterface, licenseID string, fileID string) (*VerificationResult, error) {
 	result := new(VerificationResult)
-	result.Valid = false
-	result.setVerificationResult(VERIFICATION_REASON_UNKNOWN)
+	result.Valid = true
+
+	existing, getError := ctx.GetStub().GetState(licenseID)
+
+	if getError != nil {
+		return nil, fmt.Errorf("Unable to fetch data from world state")
+	}
+
+	if existing == nil {
+		return nil, fmt.Errorf("License with id %s does not exist", licenseID)
+	}
+
+	license := new(License)
+
+	unmarshalError := json.Unmarshal(existing, license)
+	if unmarshalError != nil {
+		return nil, fmt.Errorf("Data retrieved from world state for key %s was not of type License", licenseID)
+	}
+
+	if time.Now().Before(license.Expiration) {
+		result.Valid = true
+		result.setVerificationResult(VERIFICATION_REASON_VALID_SUCCESS)
+	} else {
+		result.Valid = false
+		result.setVerificationResult(VERIFICATION_REASON_UNKNOWN)
+	}
+
 
 	return result, nil
 }
