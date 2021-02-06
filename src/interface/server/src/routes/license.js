d732159e4ddbfd93b3d4faf896655f5217ed9eb8
Aaron Dodd <ad19abg@herts.ac.uk>
2021-02-06 11:09:34 +0000

    support verification on the backend

--- src/interface/server/src/routes/license.js
+++ src/interface/server/src/routes/license.js
@@ -5,6 +5,10 @@ const path = require("path");
 
 var verifyToken = require("../middleware/auth/verify-token");
 
+var fabricCAClient = require("fabric-ca-client");
+var fabricCommon = require("fabric-common");
+var fabricNetwork = require("fabric-network");
+
 const configPath = path.join(__dirname, "..", "..", "config", "config.json");
 const configJSON = fs.readFileSync(configPath, "utf8");
 const config = JSON.parse(configJSON);
@@ -14,8 +18,10 @@ const ccpJSON = fs.readFileSync(ccpPath, "utf8");
 const ccp = JSON.parse(ccpJSON);
 
 router.post("/verify",
+    verifyToken,
     async (req, res) => {
         const licenseKey = req.body.licensekey;
+        console.log(licenseKey);
 
         const walletPath = path.join(__dirname, "..", "..", "wallet");
         const wallet = await fabricNetwork.Wallets.newFileSystemWallet(walletPath);
@@ -28,10 +34,17 @@ router.post("/verify",
             const network = await gateway.getNetwork("default-channel");
             const contract = network.getContract("license");
     
-            let transaction = contract.createTransaction("VerificationContract:VerifyLicense");
-            let transactionResponse = await transaction.evaluate(licenseKey);
-            res.send(transactionResponse);
-            return;
+            let transaction = contract.createTransaction("VerificationContract:VerifyOwnership");
+            try {
+                let transactionResponse = await transaction.evaluate(licenseKey,"file");
+                console.log(transactionResponse);
+                res.send(transactionResponse);
+                return;
+            } catch (error) {
+                console.log(error);
+                res.send(error.payload);
+                return;
+            }
         }
 
         res.sendStatus(403);
